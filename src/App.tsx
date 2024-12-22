ed28de956d78c97baab2825f438d212784e9e423
Joowon Ko <hoochoo224@gmail.com>
2024-12-22 23:50:46 +0900

    update src/App.tsx: 잘못된 path 고침

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-import MainPage from 'src/pages/index/index'
+import MainPage from './pages/index/index'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
       <Routes>
