3f59b17e6994f64c60d17871dfbc70264c5bd40d
Joowon Ko <hoochoo224@gmail.com>
2024-12-22 23:57:53 +0900

    update src/pages/contracts.ts: .toString

--- src/pages/contract.ts
+++ src/pages/contract.ts
@@ -3,7 +3,7 @@ const API_BASE_URL = 'https://port-0-ecoin-backend-m3x1zrsv043003ce.sel4.cloudty
 export const grantRoleMinter = async (account: string, amount: number)=>{
   const formData = new FormData();
   formData.append('account', account);
-  formData.append('amount', amount);
+  formData.append('amount', amount.toString());
 
   await fetch(`${API_BASE_URL}/api/grant-role-minter`, {
     method: 'POST',
@@ -14,7 +14,7 @@ export const grantRoleMinter = async (account: string, amount: number)=>{
 export const mint = async (to: string, amount: number) => {
   const formData = new FormData();
   formData.append('to', to);
-  formData.append('amount', amount);
+  formData.append('amount', amount.toString());
 
   await fetch(`${API_BASE_URL}/api/mint`, {
     method: 'POST',
