e3131143c50cd7ec71d979d1cd438ca5d3b6de06
Joowon Ko <hoochoo224@gmail.com>
2024-12-22 23:39:56 +0900

    add src/pages/contract.ts | update src/pages/index/index.tsx

---
+++ src/contract.ts
@@ -0,0 +1,23 @@
+const API_BASE_URL = 'https://port-0-ecoin-backend-m3x1zrsv043003ce.sel4.cloudtype.app/';
+
+export const grantRoleMinter = async (account: string, amount: number)=>{
+  const formData = new FormData();
+  formData.append('account', account);
+  formData.append('amount', amount);
+
+  await fetch(`${API_BASE_URL}/api/grant-role-minter`, {
+    method: 'POST',
+    body: formData
+  });
+};
+
+export const mint = async (to: string, amount: number) => {
+  const formData = new FormData();
+  formData.append('to', to);
+  formData.append('amount', amount);
+
+  await fetch(`${API_BASE_URL}/api/mint`, {
+    method: 'POST',
+    body: formData
+  })
+};
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,26 @@
+import {grantRoleMinter, mint} from "../contract"
+import {useState} from 'react';
+
 const index = ()=>{
+  const [addr, setAddr] = useState();
+  const [amount, setAmount] = useState();
+
+  const handleAddrChange = (e: any) => {
+    setAddr(e.target.value);
+  };
+  const handleAmountChange = (e: any) => {
+    setAmount(e.target.value);
+  };
+  const handleSubmit = async () => {
+    await grantRoleMinter(addr, amount);
+  };
+
   return (
     <div>
-      Hello
+      <span>발행 권한 주기(관리자만 가능)</span>
+      <input type="text" placeholder="주소를 입력하세요." onChange={handleAddrChange} />
+      <input type="text" placeholder="Ecoin 양을 입력하세요." onChange={handleAmountChange} />
+      <button onClick={handleSubmit}></button>
     </div>
   )
 };
