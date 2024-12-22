062741be51bd7bc9a6238a7228900f6856655592
Joowon Ko <hoochoo224@gmail.com>
2024-12-23 00:06:18 +0900

    update src/pages/contract.ts: json

--- src/pages/contract.ts
+++ src/pages/contract.ts
@@ -1,23 +1,37 @@
 const API_BASE_URL = 'https://port-0-ecoin-backend-m3x1zrsv043003ce.sel4.cloudtype.app/';
 
 export const grantRoleMinter = async (account: string, amount: number)=>{
-  const formData = new FormData();
-  formData.append('account', account);
-  formData.append('amount', amount.toString());
-
+  // const formData = new FormData();
+  // formData.append('account', account);
+  // formData.append('amount', amount.toString());
+  //
+  // await fetch(`${API_BASE_URL}/api/grant-role-minter`, {
+  //   method: 'POST',
+  //   body: formData
+  // });
   await fetch(`${API_BASE_URL}/api/grant-role-minter`, {
     method: 'POST',
-    body: formData
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ account, amount }), // JSON으로 전송
   });
 };
 
 export const mint = async (to: string, amount: number) => {
-  const formData = new FormData();
-  formData.append('to', to);
-  formData.append('amount', amount.toString());
-
+  // const formData = new FormData();
+  // formData.append('to', to);
+  // formData.append('amount', amount.toString());
+  //
+  // await fetch(`${API_BASE_URL}/api/mint`, {
+  //   method: 'POST',
+  //   body: formData
+  // })
   await fetch(`${API_BASE_URL}/api/mint`, {
     method: 'POST',
-    body: formData
-  })
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ to, amount }), // JSON으로 전송
+  });
 };
