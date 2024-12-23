82105b975bd41ffa27094059a9f8bbaf97a5e65d
Joowon Ko <hoochoo224@gmail.com>
2024-12-23 09:16:07 +0900

    update src/pages/index/index.tsx | update src/pages/contract

--- src/contract.ts
+++ src/contract.ts
@@ -1,6 +1,6 @@
 const API_BASE_URL = 'https://port-0-ecoin-backend-m3x1zrsv043003ce.sel4.cloudtype.app/';
 
-export const grantRoleMinter = async (account: string, amount: number)=>{
+export const grantRoleMinter = async (account: string, amount: string)=>{
   const formData = new FormData();
   formData.append('account', account);
   formData.append('amount', amount);
@@ -11,7 +11,7 @@ export const grantRoleMinter = async (account: string, amount: number)=>{
   });
 };
 
-export const mint = async (to: string, amount: number) => {
+export const mint = async (to: string, amount: string) => {
   const formData = new FormData();
   formData.append('to', to);
   formData.append('amount', amount);
