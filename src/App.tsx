64c04ac7d11a403f76dc96399dd35dd2568fc085
Joowon Ko <hoochoo224@gmail.com>
2024-12-23 10:19:27 +0900

    update src/App.tsx: add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import MainPage from './pages/index/index'
+import Minter from './pages/minter/index'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index path='/' element={<MainPage />}></Route>
+        <Route path='/minter' element={<Minter/>}></Route>
       </Routes>
     </BrowserRouter>
   )
